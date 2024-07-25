0a7d9db52ed26bfc8f5f830048d9114d4215661f
GrinyaNet <igrinya@yahoo.com>
2024-07-25 13:58:26 +0300

    calendar

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { days } from '../../utils/dateUtils.js';
 
 const Navigation = ({ weekDates }) => {
+  let currentDate = new Date();
   return (
     <header className="calendar__header">
       {weekDates.map((dayDate) => (
         <div className="calendar__day-label day-label">
           <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
-          <span className="day-label__day-number">{dayDate.getDate()}</span>
+          {dayDate.getDate() === currentDate.getDate() ? <span className="day-label__day-number day-label__day-color">{dayDate.getDate()}</span> : <span className="day-label__day-number">{dayDate.getDate()}</span>}
         </div>
       ))}
     </header>
